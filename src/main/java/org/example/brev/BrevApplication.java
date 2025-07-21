450b3c611ed627d9dd020e516fbd23c65bcdf2d2
WaiSiHnyun <waisihnyun27@gmail.com>
2025-07-21 21:54:50 +0630

    init project

---
+++ src/main/java/org/example/brev/BrevApplication.java
@@ -0,0 +1,13 @@
+package org.example.brev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/brev/BrevApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.brev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
